e23a0e5707c0e17b8b43da2d0e18d4a678dc0bb8
DonyLouis <donyl3289@gmail.com>
2024-05-19 20:14:16 +0900

    4番目のコミット

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,11 +14,58 @@ module.exports = {
 	theme: 'vdoing',
 	themeConfig: {
 		smoothScroll: true,
-        displayAllHeaders: true,
-        sidebar: 'auto',
+		displayAllHeaders: true,
+		sidebar: 'auto',
 		logo: '/assets/img/logo.png',
 		nav: [
 			{ text: 'ホーム', link: '/' },
+			{
+				text: 'フロントエンド',
+				link: '/frontend/',
+				items: [
+					{
+						text: 'フロンエンドの文章',
+						items: [
+							{
+								text: 'JavaScript',
+								link: '/pages/a61298/',
+							},
+						],
+					},
+					{
+						text: '学习笔记',
+						items: [
+							{
+								text: '《JavaScript教程》',
+								link: '/note/javascript/',
+							},
+							{
+								text: '《JavaScript高级程序设计》',
+								link: '/note/js/',
+							},
+							{ text: '《ES6 教程》', link: '/note/es6/' },
+							{ text: '《Vue》', link: '/note/vue/' },
+							{ text: '《React》', link: '/note/react/' },
+							{
+								text: '《TypeScript 从零实现 axios》',
+								link: '/note/typescript-axios/',
+							},
+							{
+								text: '《Git》',
+								link: '/note/git/',
+							},
+							{
+								text: 'TypeScript',
+								link: '/pages/51afd6/',
+							},
+							{
+								text: 'JS设计模式总结',
+								link: '/pages/4643cd/',
+							},
+						],
+					},
+				],
+			},
 			{ text: 'ブログ', link: '/blog/demo/' },
 			{ text: 'ITの単語', link: '/itWord/demo/' },
 			{ text: 'ITのテスト', link: '/itWord/demo/' },
@@ -41,11 +88,11 @@ module.exports = {
 				],
 			},
 		],
-		sidebar: { 
-            mode: 'structuring', 
-            collapsable: false, 
-        },
-        sidebarOpen: true
+		sidebar: {
+			mode: 'structuring',
+			collapsable: false,
+		},
+		sidebarOpen: true,
 		/*
           sidebar: [
             {
